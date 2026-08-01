eb0aaa8dc2bd6f315bcc267572c7c4ca7e6b2cfb
Eric Cornelissen <ericornelissen@gmail.com>
2026-08-01 22:16:17 +0200

    wip

--- .github/actions/ghasum-node/ghasum.js
+++ .github/actions/ghasum-node/ghasum.js
@@ -1,4 +1,4 @@
-import { spawnSync } from 'node:child_process';
+import { spawnSync } from "node:child_process";
 import { arch, platform } from "node:os";
 import { env, exit } from "node:process";
 
@@ -13,14 +13,14 @@ const OS = platform().toLowerCase();
 const WORKFLOW = env.GITHUB_WORKFLOW_REF.replace(`${env.GITHUB_REPOSITORY}/`, "").split("@")[0];
 const JOB = env.GITHUB_JOB;
 
-let TMP;
+let archive;
 switch (`${OS}-${ARCH}`) {
-case "linux-arm64":   TMP = "ghasum_linux_arm64.tar.gz";  break;
-case "linux-x64":     TMP = "ghasum_linux_amd64.tar.gz";  break;
-case "macos-arm64":   TMP = "ghasum_darwin_arm64.tar.gz"; break;
-case "macos-x64":     TMP = "ghasum_darwin_amd64.tar.gz"; break;
-case "windows-arm64": TMP = "ghasum_windows_arm64.zip";   break;
-case "windows-x64":   TMP = "ghasum_windows_amd64.zip";   break;
+case "linux-arm64":   archive = "ghasum_linux_arm64.tar.gz";  break;
+case "linux-x64":     archive = "ghasum_linux_amd64.tar.gz";  break;
+case "macos-arm64":   archive = "ghasum_darwin_arm64.tar.gz"; break;
+case "macos-x64":     archive = "ghasum_darwin_amd64.tar.gz"; break;
+case "windows-arm64": archive = "ghasum_windows_arm64.zip";   break;
+case "windows-x64":   archive = "ghasum_windows_amd64.zip";   break;
 }
 
 // --- Inputs ------------------------------------------------------------------
@@ -38,9 +38,9 @@ try {
 	exec(["mkdir", "-p", cwd]);
 	exec(["gh", "release", "download", VERSION, "--repo", REPOSITORY, "--pattern", CHECKSUM_FILE], { cwd });
 	exec(["shasum", "-a", "256", "-c", "-"], { cwd, input: `${CHECKSUM}  ${CHECKSUM_FILE}` });
-	exec(["gh", "release", "download", VERSION, "--repo", REPOSITORY, "--pattern", TMP], { cwd });
+	exec(["gh", "release", "download", VERSION, "--repo", REPOSITORY, "--pattern", archive], { cwd });
 	exec(["shasum", "--check", "--ignore-missing", CHECKSUM_FILE], { cwd });
-	exec(["tar", "-xf", TMP], { cwd });
+	exec(["tar", "-xf", archive], { cwd });
 
 	if (MODE === "verify") {
 		exec(["./ghasum", "verify", "-cache", "/home/runner/work/_actions", "-no-evict", "-offline", `${WORKFLOW}:${JOB}`], { cwd });
